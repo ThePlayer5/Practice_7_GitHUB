e954f6d7952cc7c3a8facba4dad58b510e4a9d47
212301 <212301@WINMAC17>
2025-02-14 11:09:45 +0300

    Добавил метод, пытаюсь присвоить запись категории

--- Practice_7_GitHUB/Program.cs
+++ Practice_7_GitHUB/Program.cs
@@ -10,8 +10,41 @@ namespace Practice_7_GitHUB
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bububbubububububbububbbububb");
+            List<double> list = new List<double>();
+            Dictionary<string, List<double>> categories = new Dictionary<string, List<double>>()
+            {
+                {"Доход", null },
+                {"Продукты", null },
+                {"Транспорт", null },
+                {"Развлечения", null }
+            };
+            Console.WriteLine("Добро пожаловать в систему управления финансами!");
+            Console.WriteLine("\n1. Добавить доход/расход  \n2. Показать отчет  \n3. Рассчитать баланс  \n4. Прогноз на следующий месяц  \n5. Статистика\n6. Выход  \n");
+            Console.Write("\nВыберите действие: ");
+            int action = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите категорию (Доход, Продукты, Транспорт, Развлечения): ");
+            string str = Console.ReadLine();
+            Console.Write("Введите сумму: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+            switch (str)
+            {
+                case "Доход":
+                    categories["Доход"] = list.Add(x);
+                    break;
+                case "Продукты":
+                    break;
+                case "Транспорт":
+                    break;
+                case "Развлечения":
+                    break;
+            }
+
             Console.ReadKey();
+        }
+        public static int AddTransaction(double x)
+        {
+            
+            return x;
         }
     }
 }
