2cbf8b9912ada6051781012db951089ba7ac74fb
GOOGLE228 <75929954+GOOGLE228@users.noreply.github.com>
2025-02-16 17:23:39 +0300

    Сделал 1 и 2 этапы, сделал вывод значений  в записях

--- Practice_7_GitHUB/Program.cs
+++ Practice_7_GitHUB/Program.cs
@@ -10,40 +10,58 @@ namespace Practice_7_GitHUB
     {
         static void Main(string[] args)
         {
-            List<double> list = new List<double>();
             Dictionary<string, List<double>> categories = new Dictionary<string, List<double>>()
             {
-                {"Доход", null },
-                {"Продукты", null },
-                {"Транспорт", null },
-                {"Развлечения", null }
+                {"Доход", new List<double>() },
+                {"Продукты", new List<double>() },
+                {"Транспорт", new List<double>() },
+                {"Развлечения", new List<double>() }
             };
             Console.WriteLine("Добро пожаловать в систему управления финансами!");
-            Console.WriteLine("\n1. Добавить доход/расход  \n2. Показать отчет  \n3. Рассчитать баланс  \n4. Прогноз на следующий месяц  \n5. Статистика\n6. Выход  \n");
-            Console.Write("\nВыберите действие: ");
-            int action = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Введите категорию (Доход, Продукты, Транспорт, Развлечения): ");
-            string str = Console.ReadLine();
-            Console.Write("Введите сумму: ");
-            double x = Convert.ToInt32(Console.ReadLine());
-            switch (str)
+            
+            for (int i = 0; i < 3; i++)
             {
-                case "Доход":
-                    categories["Доход"] = list.Add(x);
-                    break;
-                case "Продукты":
-                    break;
-                case "Транспорт":
-                    break;
-                case "Развлечения":
-                    break;
+                Console.WriteLine("\n1. Добавить доход/расход  \n2. Показать отчет  \n3. Рассчитать баланс  \n4. Прогноз на следующий месяц  \n5. Статистика\n6. Выход  \n");
+                Console.Write("\nВыберите действие: ");
+                int action = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите категорию (Доход, Продукты, Транспорт, Развлечения): ");
+                string category = Console.ReadLine();
+                Console.Write("Введите сумму: ");
+                double x = Convert.ToDouble(Console.ReadLine());
+                AddTransaction(x, category, categories);
+                Console.WriteLine("Запись добавлена.");
+                //switch (category)
+                //{
+                //    case "Доход":
+                //        AddTransaction(x, category, categories);
+                //        Console.WriteLine("Запись добавлена.");
+                //        break;
+                //    case "Продукты":
+                //        break;
+                //    case "Транспорт":
+                //        break;
+                //    case "Развлечения":
+                //        break;
+                //}
+            }
+            foreach (var sum in categories["Доход"])
+            {
+                Console.WriteLine(sum);
             }
 
+            foreach (var sum in categories["Продукты"])
+            {
+                Console.WriteLine(sum);
+            }
+            foreach (var sum in categories["Транспорт"])
+            {
+                Console.WriteLine(sum);
+            }
             Console.ReadKey();
         }
-        public static int AddTransaction(double x)
+        public static double AddTransaction(double x, string category, Dictionary<string, List<double>> categories)
         {
-            
+            categories[category].Add(x);
             return x;
         }
     }
