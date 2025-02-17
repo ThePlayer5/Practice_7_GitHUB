a50b1a538bb3238750a9fee67697c98acdce37e4
GOOGLE228 <75929954+GOOGLE228@users.noreply.github.com>
2025-02-17 10:17:32 +0300

    Сделал этап 3, добавил вывод суммы всех доходов и расходов по категориям

--- Practice_7_GitHUB/Program.cs
+++ Practice_7_GitHUB/Program.cs
@@ -17,52 +17,72 @@ namespace Practice_7_GitHUB
                 {"Транспорт", new List<double>() },
                 {"Развлечения", new List<double>() }
             };
+            double x = 0;
             Console.WriteLine("Добро пожаловать в систему управления финансами!");
-            
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine("\n1. Добавить доход/расход  \n2. Показать отчет  \n3. Рассчитать баланс  \n4. Прогноз на следующий месяц  \n5. Статистика\n6. Выход  \n");
                 Console.Write("\nВыберите действие: ");
                 int action = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Введите категорию (Доход, Продукты, Транспорт, Развлечения): ");
-                string category = Console.ReadLine();
-                Console.Write("Введите сумму: ");
-                double x = Convert.ToDouble(Console.ReadLine());
-                AddTransaction(x, category, categories);
-                Console.WriteLine("Запись добавлена.");
-                //switch (category)
-                //{
-                //    case "Доход":
-                //        AddTransaction(x, category, categories);
-                //        Console.WriteLine("Запись добавлена.");
-                //        break;
-                //    case "Продукты":
-                //        break;
-                //    case "Транспорт":
-                //        break;
-                //    case "Развлечения":
-                //        break;
-                //}
+                switch (action)
+                {
+                    case 1:
+                        Console.Write("Введите категорию (Доход, Продукты, Транспорт, Развлечения): ");
+                        string category = Console.ReadLine();
+                        Console.Write("Введите сумму: ");
+                        x = Convert.ToDouble(Console.ReadLine());
+                        AddTransaction(x, category, categories);
+                        Console.WriteLine("Запись добавлена.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Финансовый отчёт:");
+                        PrintFinanceReport(categories);
+                        break;
+                    case 3:
+                        Console.WriteLine(3);
+                        break;
+                    case 4:
+                        Console.WriteLine(4);
+                        break;
+                    case 5:
+                        Console.WriteLine(5);
+                        break;
+                    case 6:
+                        Console.WriteLine(6);
+                        break;
+                }
             }
-            foreach (var sum in categories["Доход"])
-            {
-                Console.WriteLine(sum);
-            }
+            //foreach (var sum in categories["Доход"])
+            //{
+            //    Console.WriteLine(sum);
+            //}
 
-            foreach (var sum in categories["Продукты"])
-            {
-                Console.WriteLine(sum);
-            }
-            foreach (var sum in categories["Транспорт"])
-            {
-                Console.WriteLine(sum);
-            }
             Console.ReadKey();
         }
         public static double AddTransaction(double x, string category, Dictionary<string, List<double>> categories)
         {
             categories[category].Add(x);
             return x;
+        }
+        public static Dictionary<string, List<double>> PrintFinanceReport(Dictionary<string, List<double>> categories)
+        {
+            if (categories["Доход"].Count() != 0)
+            {
+                Console.WriteLine($"Доход: {categories["Доход"].Sum()} руб. - {categories["Доход"].Count()} операций");
+            }
+            if (categories["Продукты"].Count() != 0)
+            {
+                Console.WriteLine($"Продукты: {categories["Продукты"].Sum()} руб. - {categories["Продукты"].Count()} операций");
+            }
+            if (categories["Транспорт"].Count() != 0)
+            {
+                Console.WriteLine($"Транспорт: {categories["Транспорт"].Sum()} руб. - {categories["Транспорт"].Count()} операций");
+            }
+            if (categories["Развлечения"].Count() != 0)
+            {
+                Console.WriteLine($"Развлечения: {categories["Развлечения"].Sum()} руб. - {categories["Развлечения"].Count()} операций");
+            }
+            return categories;
         }
     }
 }
